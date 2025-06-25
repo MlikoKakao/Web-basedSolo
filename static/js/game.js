b26e511665c51e62dc8e86ce5d4f822bbf8a80a4
PC <mattewczlp@seznam.cz>
2025-06-25 16:43:20 +0200

    Interactive HP bars, TODO: buttons, edit picture on retro-style HP bar, how do I make this game actually function in rounds?

--- static/js/game.js
+++ static/js/game.js
@@ -3,7 +3,9 @@ function attack() {
         .then(response => response.json())
         .then(data => {
             document.getElementById("enemy-hp").textContent = data.enemy_hp;
-            document.getElementById("attackBut").textContent = `You hit for ${data.dmg} damage!`;
+            document.getElementById("damageNotif").textContent = `You hit for ${data.dmg} damage!`;
+            let enemyHealth = document.getElementById("enemyHPBar")
+            enemyHealth.value = data.enemy_hp;
         });
 }
 
@@ -11,7 +13,21 @@ function fireball(){
     fetch("/fireball", {method:"POST"})
         .then(response => response.json())
         .then(data => {
-            document.getElementById("enemy-hp").textContent = data.enemy_hp;
-            document.getElementById("fireballBut").textContent = `Fireball hit for ${data.dmg} damage!`;
+            const currentHP = data.enemy_hp;
+                const maxHP = data.enemy_max_hp || 100;
+                const fullBarWidth = 270;
+
+                document.getElementById("enemy-hp").textContent = currentHP;
+                document.getElementById("damageNotif").textContent = `Fireball hit for ${data.dmg} damage!`;
+                document.getElementById("enemyHPBar").value = currentHP;
+
+                const newWidthPx = (currentHP / maxHP) * fullBarWidth;
+                const hpFill = document.getElementById("hp-fill");
+                hpFill.style.width = newWidthPx + "px";
             })
+}
+
+function updateHP(percent) {
+    const hpFill = document.querySelector('.hp-fill');
+    hpFill.style.width = percent + '%';
 }
